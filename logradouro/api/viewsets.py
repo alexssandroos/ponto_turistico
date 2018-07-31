da3abf70082e9c23ab96a4b129c5d3186c1cce82
Alexssandro <Alexsssandroos@yahoo.com.br>
2018-07-30 22:48:55 -0300

    Incremento de Buscas no endpoint de enderecos

--- logradouro/api/viewsets.py
+++ logradouro/api/viewsets.py
@@ -1,4 +1,5 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.filters import SearchFilter
 from logradouro.models import Logradouro
 from .serializers import LogradouroSerializer
 
@@ -6,3 +7,5 @@ class LogradouroViewSet(ModelViewSet):
 
     queryset = Logradouro.objects.all()
     serializer_class = LogradouroSerializer
+    filter_backends = (SearchFilter,)
+    search_fields = ('logradouro', 'cidade')

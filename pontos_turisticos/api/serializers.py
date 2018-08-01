cd4a0ef89af22f4f56f3bbee6e882650fe58d305
Alexssandro <Alexsssandroos@yahoo.com.br>
2018-07-31 22:50:17 -0300

    Ajuste no endpoint de pontos turisticos NESTED

--- pontos_turisticos/api/serializers.py
+++ pontos_turisticos/api/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework.serializers import ModelSerializer
 from pontos_turisticos.models import PontoTuristico
+from logradouro.api.serializers import LogradouroSerializer
+from atracoes.api.serializers import AtracaoSerializer
+
 
 class PontoTuristicoSerializer(ModelSerializer):
-    class Meta:
-        model = PontoTuristico
-        fields = ('id', 'nome', 'descricao', 'aprovado', 'foto')
+	logradouro = LogradouroSerializer()
+	atracao = AtracaoSerializer(many=True)
+
+	class Meta:
+		model = PontoTuristico
+		fields = ('id', 'nome', 'descricao', 'aprovado', 'foto',
+        	'logradouro', 'atracao')
